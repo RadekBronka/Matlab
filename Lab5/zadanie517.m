6efa23f632507928cd8ac53659d0729bc3da6a1b
RadekBronka <radoslavb12@gmail.com>
2024-11-06 12:58:41 +0100

    Create zadanie517.m

---
+++ Lab5/zadanie517.m
@@ -0,0 +1,21 @@
+% interp_obiekt3D.m
+clear all; close all;
+
+%load('X.mat');
+ load('babia_gora.dat'); X=babia_gora;
+
+figure; grid; plot3( X(:,1), X(:,2), X(:,3), 'b.' ); pause
+
+x = X(:,1); y = X(:,2); z = X(:,3);            % pobranie x,y,z
+xvar = min(x) : (max(x)-min(x))/200 : max(x);  % zmiennosc x
+yvar = min(y) : (max(y)-min(y))/200 : max(y);  % zmiennosc y
+
+[Xi,Yi] = meshgrid( xvar, yvar );              % siatka interpolacji xi, yi
+for(i={'nearest', 'linear','cubic', 'spline'})
+out = griddata( x, y, z, Xi,Yi, i{1} );     % interp: nearest, linear, spline, cubic
+figure; surf( out ); pause                     % rysunek             
+figure; mesh( out ); pause                     % rysunek   
+end
+
+
+%nearest najgorzej, bo interpoluje najbliższym znanym sąsiadem- stąd skoki
