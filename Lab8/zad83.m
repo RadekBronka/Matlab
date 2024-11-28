df4c63596b17a98132f5ef89c490bd9c898217e2
RadekBronka <radoslavb12@gmail.com>
2024-11-28 09:59:33 +0100

    Update zad83.m

--- Lab8/zad83.m
+++ Lab8/zad83.m
@@ -12,9 +12,26 @@ for i = 1:20                      % poczatek petli
     [ymax,imax] = max(abs(y));    % najwieksza wartosc abs() i jej indeks
     x = y/ymax;                   % wektor wlasny (noramlizaca wektora y przez największą wartość bezwzględną)
     lambda = ymax;                % wartosc wlasna ymax=y(imax)
+    
 end                               % koniec petli
+%x = x / norm(x); %normalizacja x w formie euklidesowej tak jak w funkcji eig
 x, lambda,                        % pokaz wynik: max wartosc wlasna i wektor wlasny
 [ V, D ] = eig(A)                 % porownaj z funkcja Matlaba
     
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+%dlaczego inaczej?
+%inna normalziacja w eig
