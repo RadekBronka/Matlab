2ce4cd6bd120b9b1419785681374374576264b89
RadekBronka <radoslavb12@gmail.com>
2024-12-04 18:14:25 +0100

    Update zadanie.m

--- Lab9/zadanie.m
+++ Lab9/zadanie.m
@@ -6,69 +6,56 @@ a  = pi-pi/5; b=pi+pi/5;  % znajdz zero funkcji y=sin(x) dla x=pi
 f  = @(x) sin(x);         % definicja funkcji
 fp = @(x) cos(x);         % definicja pochodnej funkcji
 tol=0.001*pi;
-x = 0 : 0.01 : 2*pi;
-
-%sinus
-disp("Sinus")
-cb = nonlinsolvers( f, fp, a, b, 'bisection', it,tol );
-cr = nonlinsolvers( f, fp, a, b, 'regula-falsi', it,tol);
-cn = nonlinsolvers( f, fp, a, b, 'newton-raphson', it,tol);
+z=pi;
+disp("sinus");
+cb = nonlinsolvers( f, fp, a, b, 'bisection', it,tol,z);
+cr = nonlinsolvers( f, fp, a, b, 'regula-falsi', it,tol,z);
+cn = nonlinsolvers( f, fp, a, b, 'newton-raphson', it,tol,z);
 figure; plot( 1:it,cb,'o-', 1:it,cr,'*', 1:it,cn,'^-'); xlabel('iter'); title('c(iter)')
 grid on, legend('Bisection','Regula-Falsi','Newton-Raphson');
 
-%kwadratowa
-%funkcja 2   kwadratowa   
-% Nachylenia: 
-% 45 stopni-> b=1
-%5 stopni -> b=0.087
-%80 stopni-> b=5.67
-a = 1; % współczynnik kwadratowy
-c = 1; % współczynnik wolny
-b_values = [5.67,1,0.087]; % różne wartości b
-it=20;
+a = 0.5; % współczynnik kwadratowy
+c = 0; % współczynnik wolny
+z=0;
+it=16;
 a0=-1;
 b0=2;
 tol1=0.001;
-
-% Kolory dla wykresów
-colors = {'r', 'g', 'b'}; % różne kolory dla różnych b
-markers = {'o-', '*', '^-'}; % różne znaczniki dla metod
-
-% Wykres funkcji kwadratowych
-x = linspace(a0, b0, 100); % przedział dla wykresów funkcji
-figure;
-hold on;
-for id = 1:length(b_values)
-    b = b_values(id);
-    f = @(x) a*x.^2 + b*x + c; % definicja funkcji
-    plot(x, f(x), 'Color', colors{id}, 'DisplayName', sprintf('f(x) dla b=%.3f', b));
-end
-
-figure;
-hold on;
-disp("Kwadratowa")
-for id=1:length(b_values);
-    b=b_values(id); 
-    f=@(x) a*x.^2+b*x+c;
-    fp=@(x) 2*a*x +b;
-
-    % Znajdowanie miejsc zerowych
-    disp(b);
-    cb = nonlinsolvers(f, fp, a0, b0, 'bisection', it,tol1);
-    cr = nonlinsolvers(f, fp, a0, b0, 'regula-falsi', it,tol1);
-    cn = nonlinsolvers(f, fp, a0, b0, 'newton-raphson', it,tol1);
-
-    % Wykres zbieżności dla danej wartości b
-    plot(1:it, cb, markers{1}, 'Color', colors{id}, 'DisplayName', sprintf('Bisection, b=%.3f', b));
-    plot(1:it, cr, markers{2}, 'Color', colors{id}, 'DisplayName', sprintf('Regula-Falsi, b=%.3f', b));
-    plot(1:it, cn, markers{3}, 'Color', colors{id}, 'DisplayName', sprintf('Newton-Raphson, b=%.3f', b));
-end
-    
+b=5.67; % różne wartości b
+f=@(x) a*x.^2+b*x+c;
+fp=@(x) 2*a*x +b;
 
 
-xlabel('Iteracje');
-ylabel('c(iter)');
-title('Zbieżność metod iteracyjnych dla różnych wartości b');
-grid on;
-legend('show', 'Location', 'best'); % automatyczne generowanie legendy
-hold off;
+disp("kwadratowa, b=5.67");
+cb = nonlinsolvers(f, fp, a0, b0, 'bisection', it,tol1,z);
+cr = nonlinsolvers(f, fp, a0, b0, 'regula-falsi', it,tol1,z);
+cn = nonlinsolvers(f, fp, a0, b0, 'newton-raphson', it,tol1,z);
+
+figure; plot( 1:it,cb,'o-', 1:it,cr,'*', 1:it,cn,'^-'); xlabel('iter'); title('c(iter), b=5.67')
+grid on, legend('Bisection','Regula-Falsi','Newton-Raphson');
+
+b=1; % różne wartości b
+f=@(x) a*x.^2+b*x+c;
+fp=@(x) 2*a*x +b;
+
+
+disp("kwadratowa, b=1");
+cb = nonlinsolvers(f, fp, a0, b0, 'bisection', it,tol1,z);
+cr = nonlinsolvers(f, fp, a0, b0, 'regula-falsi', it,tol1,z);
+cn = nonlinsolvers(f, fp, a0, b0, 'newton-raphson', it,tol1,z);
+
+figure; plot( 1:it,cb,'o-', 1:it,cr,'*', 1:it,cn,'^-'); xlabel('iter'); title('c(iter), b=1')
+grid on, legend('Bisection','Regula-Falsi','Newton-Raphson');
+
+b=0.087; % różne wartości b
+f=@(x) a*x.^2+b*x+c;
+fp=@(x) 2*a*x +b;
+
+
+disp("kwadratowa, b=0.087");
+cb = nonlinsolvers(f, fp, a0, b0, 'bisection', it,tol1,z);
+cr = nonlinsolvers(f, fp, a0, b0, 'regula-falsi', it,tol1,z);
+cn = nonlinsolvers(f, fp, a0, b0, 'newton-raphson', it,tol1,z);
+
+figure; plot( 1:it,cb,'o-', 1:it,cr,'*', 1:it,cn,'^-'); xlabel('iter'); title('c(iter), b=0.087')
+grid on, legend('Bisection','Regula-Falsi','Newton-Raphson');
